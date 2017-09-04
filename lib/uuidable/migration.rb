23fe1f05179faeede86fcc94c07827fcf5b0e670
Sergey Gnuskov <sergey.gnuskov@flant.com>
2017-09-04 18:45:26 +0300

    Use unique index everywhere by default

--- lib/uuidable/migration.rb
+++ lib/uuidable/migration.rb
@@ -1,22 +1,39 @@
 module Uuidable
   COLUMN_NAME = :uuid
   COLUMN_TYPE = :binary
-  COLUMN_OPTIONS = { limit: 36, null: false, index: true }.freeze
+  COLUMN_OPTIONS = { limit: 36, null: false }.freeze
+  INDEX_OPTIONS = { unique: true }.freeze
 
   # Module adds method to table definition
   module TableDefinition
     def uuid(opts = {})
-      column COLUMN_NAME, COLUMN_TYPE, opts.merge(COLUMN_OPTIONS)
+      index_opts = opts.delete(:index)
+      column_name = opts.delete(:column_name) || COLUMN_NAME
+
+      column column_name, COLUMN_TYPE, COLUMN_OPTIONS.merge(opts)
+      index column_name, INDEX_OPTIONS.merge(index_opts) unless index_opts == false
     end
   end
 
   # Module adds method to alter table migration
   module Migration
     def add_uuid_column(table_name, opts = {})
-      add_column table_name, COLUMN_NAME, COLUMN_TYPE, opts.merge(COLUMN_OPTIONS)
+      index_opts = opts.delete(:index)
+      column_name = opts.delete(:column_name) || COLUMN_NAME
+
+      add_column table_name, column_name, COLUMN_TYPE, COLUMN_OPTIONS.merge(opts)
+
+      add_uuid_index(index_opts.merge(column_name: column_name)) unless index_opts == false
+    end
+
+    def add_uuid_index(table_name, opts = {})
+      column_name = opts.delete(:column_name) || COLUMN_NAME
+
+      add_index table_name, column_name, INDEX_OPTIONS.merge(opts)
     end
   end
 end
+
 if defined? ActiveRecord::ConnectionAdapters::TableDefinition
   ActiveRecord::ConnectionAdapters::TableDefinition.send :include, Uuidable::TableDefinition
 end
